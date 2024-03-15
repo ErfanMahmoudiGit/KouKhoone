c4d2bcf112ede1680ebcbe4ae73a283b446a185e
Erifun <erfan.mahmoudi.2001@gmail.com>
2024-03-15 17:41:52 +0330

    added limit to each article description using truncatewords

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
-#from django.http import HttpResponse , JsonResponse
 from .models import Article
-# Create your views here.
+#if you want to send HttpResponse or JsonResponse you should use the following commented code.
+#from django.http import HttpResponse , JsonResponse
 
 def home (request):
     context = {
-        "articles" : Article.objects.filter(status="p")
+        "articles" : Article.objects.filter(status="p"),
     }
     return render (request, "blog/home.html", context)
+
 def detail(request, slug):
     context = {
         "article" : Article.objects.get (slug=slug)
--- config/urls.py
+++ config/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('', include ("blog.urls")),
 ]
 
+# the following 3 lines are used to fix the bug while trying to see the images in debug mode.
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
