697a182aa15affc378f8fa2c6e5f4fc3e560a417
Erifun <erfan.mahmoudi.2001@gmail.com>
2024-03-16 22:42:17 +0330

    changed admin panel language to fa-ir

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, get_list_or_404
 from .models import Article
 #if you want to send HttpResponse or JsonResponse you should use the following commented code.
 #from django.http import HttpResponse , JsonResponse
@@ -13,6 +13,6 @@ def home (request):
 def detail(request, slug):
     context = {
         #Article.objects.get (slug=slug)
-        "article" : get_object_or_404 (Article, slug=slug)
+        "article" : get_object_or_404 (Article, slug=slug, status = "p")
     }
     return render (request, "blog/detail.html", context)
