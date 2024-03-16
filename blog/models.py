450ed89fb47a7617fbdc22da93bafb20c1364c12
Erifun <erfan.mahmoudi.2001@gmail.com>
2024-03-16 22:51:40 +0330

    changed language of article to persian.

--- blog/models.py
+++ blog/models.py
@@ -4,17 +4,21 @@ from django.utils import timezone
 
 class Article(models.Model):
     STATUS_CHOICES = (
-    ('d', 'Draft'),
-    ('p', 'Published'),
+    ('d', 'پیش نویس'),
+    ('p', 'منتشر شده'),
     )
-    title = models.CharField(max_length= 200)
-    slug = models.SlugField (max_length = 100, unique=True)
-    description = models.TextField ()
-    thumbnail = models.ImageField (upload_to="images")
-    publish = models.DateTimeField (default = timezone.now)
-    created = models.DateTimeField (auto_now_add = True)
+    title = models.CharField(max_length= 200, verbose_name = "عنوان مقاله")
+    slug = models.SlugField (max_length = 100, unique=True, verbose_name = "آدرس مقاله")
+    description = models.TextField (verbose_name = "محتوا")
+    thumbnail = models.ImageField (upload_to="images", verbose_name = " تصویر")
+    publish = models.DateTimeField (default = timezone.now, verbose_name = "زمان انتشار")
+    created = models.DateTimeField (auto_now_add = True, verbose_name = "زمان ساخت")
     updated = models.DateTimeField (auto_now = True)
-    status = models.CharField (max_length = 1, choices = STATUS_CHOICES)
+    status = models.CharField (max_length = 1, choices = STATUS_CHOICES, verbose_name = "وضعیت")
+    class Meta(object):
+        verbose_name = "مقاله"
+        verbose_name_plural = "مقاله ها"
+
 
     def __str__(self):
         return self.title
