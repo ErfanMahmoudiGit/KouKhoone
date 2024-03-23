ecb799bd1237839766586c22b2796a790d27b78d
Erifun <erfan.mahmoudi.2001@gmail.com>
2024-03-23 05:05:52 +0330

    added sub-category

--- blog/admin.py
+++ blog/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Article, category
 
 class categoryAdmin (admin.ModelAdmin):
-    list_display = ('position', 'title', 'slug', 'status')
+    list_display = ('position', 'title', 'slug', 'parent', 'status')
     list_filter = (['status'])
     search_fields = ('title','slug')
     prepopulated_fields = {'slug':('title',),}
