68003356d07cd42d52f11c31f293ae6c450d5451
Erifun <erfan.mahmoudi.2001@gmail.com>
2024-03-27 23:45:04 +0330

    added api to manage users

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path, include
-from api.views import ArticleList, ArticleDetail
+from api.views import ArticleList, ArticleDetail, UserList, UserDetail
 
 app_name = "api"
 urlpatterns = [
     path ("", ArticleList.as_view(), name = "home"),
     path ("page/<int:page>", ArticleList.as_view(), name = "home"),
     path ("<int:pk>", ArticleDetail.as_view(), name = "detail"),
+    path ("users/", UserList.as_view(), name = "userlist"),
+    path ("users/<int:pk>", UserDetail.as_view(), name = "userdetail"),
+
 ]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,7 @@
 from blog.models import Article
-from .serializers import ArticleSerializer
+from .serializers import ArticleSerializer, UserSerializer
 from rest_framework.generics import ListCreateAPIView, RetrieveAPIView
+from django.contrib.auth.models import User
 
 class ArticleList(ListCreateAPIView):
     queryset = Article.objects.all()
@@ -9,3 +10,11 @@ class ArticleList(ListCreateAPIView):
 class ArticleDetail(RetrieveAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
+
+class UserList(ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
