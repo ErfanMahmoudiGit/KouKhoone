643f60514f2da31714836093ff8cc9d94289400d
Erifun <erfan.mahmoudi.2001@gmail.com>
2024-03-27 20:37:31 +0330

    added view and url for articles detail

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from api.views import ArticleList
+from api.views import ArticleList, ArticleDetail
 
 app_name = "api"
 urlpatterns = [
     path ("", ArticleList.as_view(), name = "home"),
     path ("page/<int:page>", ArticleList.as_view(), name = "home"),
+    path ("<int:pk>", ArticleDetail.as_view(), name = "detail"),
 ]
--- api/views.py
+++ api/views.py
@@ -1,7 +1,11 @@
 from blog.models import Article
 from .serializers import ArticleSerializer
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveAPIView
 
-class ArticleList(ListAPIView):
+class ArticleList(ListCreateAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+
+class ArticleDetail(RetrieveAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
