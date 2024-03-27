636bc50b9bfe52f11dafb52c87849d12a38a4906
Erifun <erfan.mahmoudi.2001@gmail.com>
2024-03-28 01:53:59 +0330

    added permission to the api. and added permission to users api so that only admin's can see and edit users info.

--- api/views.py
+++ api/views.py
@@ -2,6 +2,7 @@ from blog.models import Article
 from .serializers import ArticleSerializer, UserSerializer
 from rest_framework.generics import ListCreateAPIView, RetrieveAPIView
 from django.contrib.auth.models import User
+from rest_framework.permissions import IsAdminUser
 
 class ArticleList(ListCreateAPIView):
     queryset = Article.objects.all()
@@ -15,7 +16,9 @@ class ArticleDetail(RetrieveAPIView):
 class UserList(ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = (IsAdminUser, )
 
 class UserDetail(RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = (IsAdminUser, )
--- config/settings.py
+++ config/settings.py
@@ -142,3 +142,9 @@ if not DEBUG:
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ]
+}
