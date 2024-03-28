87baa1b30c6e646d2ed4dfc155c6d40a8678dd32
Erifun <erfan.mahmoudi.2001@gmail.com>
2024-03-28 23:15:17 +0330

    added new permission so only the author of an article can edit the article

--- api/permissions.py
+++ api/permissions.py
@@ -7,6 +7,18 @@ class IsSuperUser(BasePermission):
 class IsStaffOrReadOnly(BasePermission):
     def has_permission (self, request, view):
         return bool(
-        request.method in SAFE_METHODES or
+        request.method in SAFE_METHODS or
         request.user and request.user.is_staff
+        )
+
+class IsAuthorOrReadOnly(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
+
+        return bool(
+            #give access to seperuser
+            request.user.is_authenticated and request.user.is_superuser or
+            #give access to author of object
+            request.user.is_authenticated and obj.author == request.user
         )
--- api/views.py
+++ api/views.py
@@ -3,15 +3,18 @@ from .serializers import ArticleSerializer, UserSerializer
 from rest_framework.generics import ListCreateAPIView, RetrieveAPIView, RetrieveUpdateAPIView
 from django.contrib.auth.models import User
 from rest_framework.permissions import IsAdminUser
-from .permissions import IsSuperUser
+from .permissions import IsSuperUser, IsAuthorOrReadOnly, IsStaffOrReadOnly
+
 class ArticleList(ListCreateAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
 
-class ArticleDetail(RetrieveAPIView):
+class ArticleDetail(RetrieveUpdateAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
     lookup_field = "slug"
+
+    permission_classes = (IsStaffOrReadOnly, IsAuthorOrReadOnly)
 
 class UserList(ListCreateAPIView):
     queryset = User.objects.all()
