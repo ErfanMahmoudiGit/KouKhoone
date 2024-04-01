a3b744a23e36df7e93a3fc94af036ad3b0de88d6
Erifun <erfan.mahmoudi.2001@gmail.com>
2024-04-01 20:20:56 +0330

    fixed password reset email bug

--- config/urls.py
+++ config/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 #from rest_framework.authtoken.views import obtain_auth_token
 #from api.views import RevokeToken
-
+from dj_rest_auth.views import PasswordResetConfirmView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include ("blog.urls")),
@@ -27,6 +27,7 @@ urlpatterns = [
     #path ('api/revoke/', RevokeToken.as_view()),
     path('api/rest-auth/', include('dj_rest_auth.urls')),
     path('api/rest-auth/registration/', include('dj_rest_auth.registration.urls')),
+    path('api/rest-auth/password/reset/confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name = 'password_reset_confirm'),
 
 
     #the following path is not needed. because i no longer use session for auth.
