e63111625c53cdefe1b836934addc0c8a7914ad5
Erifun <erfan.mahmoudi.2001@gmail.com>
2024-04-04 23:36:42 +0330

    used routers in urls.py to auto generate routes.

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,5 @@
-from django.urls import path, include
+"""
 from api.views import ArticleList, ArticleDetail, UserList, UserDetail
-
-app_name = "api"
 urlpatterns = [
     path ("", ArticleList.as_view(), name = "home"),
     path ("page/<int:page>", ArticleList.as_view(), name = "home"),
@@ -9,4 +7,19 @@ urlpatterns = [
     path ("<slug:slug>", ArticleDetail.as_view(), name = "detail"),
     path ("users/", UserList.as_view(), name = "userlist"),
     path ("users/<int:pk>", UserDetail.as_view(), name = "userdetail"),
+]
+"""
+
+from django.urls import path, include
+from rest_framework import routers
+from api.views import UserViewSet, ArticleViewSet
+
+app_name = "api"
+
+router = routers.SimpleRouter()
+router.register('users', UserViewSet, basename= 'users')
+router.register('', ArticleViewSet, basename= 'articles')
+
+urlpatterns = [
+    path ("", include(router.urls)),
 ]
